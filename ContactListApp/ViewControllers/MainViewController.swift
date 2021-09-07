850ed094f4fe8a0d3eb16b583d965180fd1dcee0
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-09-07 11:58:19 +0400

    MVC architecture, basic layout

---
+++ ContactListApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  ContactListApp
+//
+//  Created by Nadzeya Shpakouskaya on 07/09/2021.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
