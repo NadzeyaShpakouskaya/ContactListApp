bd7c17efe4ab1fa634b37aab6d543a9820281637
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-09-07 11:35:44 +0400

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Nadzeya Shpakouskaya on 07/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
